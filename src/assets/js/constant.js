e15bf3902a04ace961bed0eb0c95717452747974
gangqiang.sun <gangqiang.sun@advantech.com.cn>
2020-04-07 14:09:08 +0800

    modify release time

--- src/assets/js/constant.js
+++ src/assets/js/constant.js
@@ -2,7 +2,7 @@
 const APPLICATIONTITLE = "WISE-PaaS/Android-DM";
 
 //footer release time
-const RELEASEDATE = "20200113";
+const RELEASEDATE = "20200401";
 const POWER = "AIM-Android";
 
 //show system app in app market
